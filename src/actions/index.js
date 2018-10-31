4f1f1076344e143c2c9fbc9163389c091009071c
madisoncarr <madison.carr@utexas.edu>
2018-10-31 14:26:20 -0500

    reducer stuff

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,9 @@
-export default function selectBook(book) {
-    console.log('A book has been selected:', book.title);
+export function selectBook(book) {
+    // selectBook is an ActionCreator, it needs to return an 
+    // action with an object with a type property. 
+    return {
+        type: 'BOOK_SELECTED',
+        payload: book
+    };
 }
 
